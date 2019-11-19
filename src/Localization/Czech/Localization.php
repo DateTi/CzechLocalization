4da99b1d3f2f5a69f430bcc015a1ace62a7e8b43
Jan Galek <admin@gcore.cz>
2019-11-20 00:15:53 +0100

    Added localization and use abstract

--- src/Localization/Czech/Localization.php
+++ src/Localization/Czech/Localization.php
@@ -3,7 +3,31 @@ declare(strict_types=1);
 
 namespace DateTi\Localization\Czech;
 
-class Localization implements LocalizationInterface
-{
+use DateTi\Localization\AbstractLocalization;
+use Nette\Neon\Neon;
 
+class Localization extends AbstractLocalization
+{
+    /** @var array */
+    protected $config;
+
+    /**
+     * @return array
+     * @throws \RuntimeException
+     */
+    protected function getConfig(): array
+    {
+        if (!$this->config) {
+            $file = __DIR__ . '/cs.neon';
+            $content = file_get_contents($file);
+
+            if ($content === false) {
+                throw new \RuntimeException('Unavailable get content from file ' . $file);
+            }
+
+            $this->config = Neon::decode($content, Neon::BLOCK);
+        }
+
+        return $this->config;
+    }
 }
